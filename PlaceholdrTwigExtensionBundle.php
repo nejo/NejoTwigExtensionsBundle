c99df06e0a483bf8c3c4ee0dd510584cf8f16609
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-11-12 12:24:27 +0100

    import extension from Stilcut private project

---
+++ PlaceholdrTwigExtensionBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nejo\PlaceholdrTwigExtensionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * PlaceholdrExtensionBundle
+ *
+ * @author Alejandro Cornejo <acornejovila@gmail.com>
+ */
+class PlaceholdrTwigExtensionBundle extends Bundle
+{
+
+}
---
+++ Twig/Extension/PlaceholdrExtension.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Nejo\PlaceholdrTwigExtensionBundle\Twig\Extension;
+
+/**
+ * Class PlaceholdrExtension
+ *
+ * Twig extension for calling the service Placehold.it
+ *
+ * @link http://placehold.it/
+ *
+ * @author Alejandro Cornejo <acornejovila@gmail.com>
+ */
+class PlaceholdrExtension extends \Twig_Extension
+{
+
+    const DEFAULT_FG_COLOR = '333333';
+    const DEFAULT_FORMAT = '.jpg';
+
+    /**
+     * @var string
+     */
+    private $_url = '';
+
+    /**
+     * @var string
+     */
+    private $_backgroundColor = '';
+
+    /**
+     * @var string
+     */
+    private $_foregroundColor = '';
+
+    /**
+     * @var string
+     */
+    private $_text = '';
+
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('placeholdr', array($this, 'placeholdrUrl')),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'placeholdr_extension';
+    }
+
+    /**
+     * @param string $size
+     * @param string $text
+     * @param string $backgroundColor
+     * @param string $foregroundColor
+     * @param string $format
+     *
+     * @return string
+     */
+    public function placeholdrUrl(
+        $size,
+        $text='',
+        $backgroundColor='',
+        $foregroundColor = '',
+        $format=''
+    )
+    {
+        $this->_setUrl('http://placehold.it/' . $size);
+        $this->_setBackgroundColor($backgroundColor);
+        $this->_setForegroundColor($foregroundColor);
+        $this->_setText($text);
+
+        return $this->_getPlaceHoldrUrl();
+    }
+
+    /**
+     * @return string
+     */
+    private function _getPlaceholdrUrl()
+    {
+        $url = $this->_getUrl();
+        $url .= $this->_getBackgroundColor();
+        $url .= $this->_getForegroundColor();
+        $url .= $this->_getText();
+
+        return $url;
+    }
+
+    /**
+     * @param string $url
+     */
+    private function _setUrl($url)
+    {
+        $this->_url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    private function _getUrl()
+    {
+        return $this->_url;
+    }
+
+    /**
+     * @param string $text
+     */
+    private function _setText($text)
+    {
+        if (!empty($text)) {
+            $this->_text = $text;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function _getText()
+    {
+        $text = '';
+
+        if (!empty($this->_text)) {
+            $text = '&text=' . urlencode($this->_text);
+        }
+
+        return $text;
+    }
+
+    /**
+     * @param string $color
+     */
+    private function _setBackgroundColor($color)
+    {
+        $this->_backgroundColor = $color;
+    }
+
+    /**
+     * @return string
+     */
+    private function _getBackgroundColor()
+    {
+        return '/' . $this->_backgroundColor;
+    }
+
+    /**
+     * @param string $color
+     */
+    private function _setForegroundColor($color)
+    {
+        $this->_foregroundColor = $color;
+    }
+
+    /**
+     * @return string
+     */
+    private function _getForegroundColor()
+    {
+        return '/' . $this->_foregroundColor;
+    }
+}
