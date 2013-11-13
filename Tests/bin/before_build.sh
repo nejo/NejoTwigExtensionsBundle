26da6f7ab5cac2dd6db5f37e47283f9e6bc0299e
Alejandro Cornejo <alejandro.cornejo@splendia.com>
2013-11-13 12:43:34 +0100

    improve performance for travis

--- Tests/bin/before_build.sh
+++ Tests/bin/before_build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
 composer config -g preferred-install source
-
-sed -i -e "s/\"minimum-stability\": \"stable\"/\"minimum-stability\": \"${MIN_STABILITY}\"/" composer.json
-
-composer --no-interaction --no-update require symfony/symfony:${SYMFONY_VERSION} symfony/twig-bundle:${SYMFONY_VERSION}
-composer --no-interaction --no-update require --dev symfony/symfony:${SYMFONY_VERSION}
-composer --no-interaction update
+composer --no-interaction -o update
